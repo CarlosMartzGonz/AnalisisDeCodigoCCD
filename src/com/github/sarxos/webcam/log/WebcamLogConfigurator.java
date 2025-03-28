f21f1caa56b1f25be5ab433cc1a325b80c2a2779
dani3606 <dcazgon@g.educaand.es>
2025-03-28 14:13:26 +0100

    Ya he arreglado el 3 error

--- src/com/github/sarxos/webcam/log/WebcamLogConfigurator.java
+++ src/com/github/sarxos/webcam/log/WebcamLogConfigurator.java
@@ -1,41 +1,36 @@
 package com.github.sarxos.webcam.log;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * Configure loggers.
- * 
+ *
  * @author Bartosz Firyn (SarXos)
  */
 public class WebcamLogConfigurator {
 
-	/**
-	 * Logger instance.
-	 */
 	private static final Logger LOG = LoggerFactory.getLogger(WebcamLogConfigurator.class);
 
 	/**
 	 * Configure SLF4J.
-	 * 
+	 *
 	 * @param is input stream to logback configuration xml
 	 */
 	public static void configure(InputStream is) {
-
 		ClassLoader cl = Thread.currentThread().getContextClassLoader();
 
 		try {
-
 			String[] names = {
-				"ch.qos.logback.classic.LoggerContext",
-				"ch.qos.logback.classic.joran.JoranConfigurator",
+					"ch.qos.logback.classic.LoggerContext",
+					"ch.qos.logback.classic.joran.JoranConfigurator",
 			};
 			for (String name : names) {
 				Class.forName(name, false, cl);
@@ -58,35 +53,31 @@ public class WebcamLogConfigurator {
 
 	/**
 	 * Configure SLF4J.
-	 * 
+	 *
 	 * @param file logback configuration file
 	 */
 	public static void configure(File file) {
-		FileInputStream fis = null;
-		try {
-			fis = new FileInputStream(file);
-			configure(fis);
-		} catch (FileNotFoundException e) {
-			LOG.error("File not found " + file, e);
+		Path path = file.toPath();
+		try (InputStream is = Files.newInputStream(path)) {
+			configure(is);
+		} catch (IOException e) {
+			LOG.error("Error processing file " + file, e);
 			e.printStackTrace();
-		} finally {
-			if (fis != null) {
-				try {
-					fis.close();
-				} catch (IOException e) {
-					LOG.error("Cannot close file " + file, e);
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
 	/**
 	 * Configure SLF4J.
-	 * 
-	 * @param file logback configuration file path
+	 *
+	 * @param filePath logback configuration file path
 	 */
-	public static void configure(String file) {
-		configure(new File(file));
+	public static void configure(String filePath) {
+		Path path = Paths.get(filePath);
+		try (InputStream is = Files.newInputStream(path)) {
+			configure(is);
+		} catch (IOException e) {
+			LOG.error("Error processing file " + filePath, e);
+			e.printStackTrace();
+		}
 	}
 }
